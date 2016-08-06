302b2829e7e6bda9812627748facfcb7fa2052d0
pbl64k <pbl64k@gmail.com>
2016-08-06 18:34:40 +0300

    Eh...

--- MkProb.hs
+++ MkProb.hs
@@ -4,10 +4,10 @@ import Ori
 import System.IO
 
 main = do
-    let s1 = initxy' (1%7) 1
+    let s1 = initxy' (1%7) (11%17)
     --let s2 = reflEnd (Seg (V2 (-2%1) 0) (V2 0 (-1))) s1
     let s2 = reflEnd (Seg (V2 0 12) (V2 11 5)) s1
-    putStrLn $ showOState s2 --(smallTweakEnd s2)
+    putStrLn $ showOState (smallTweakEnd s2)
 
 --main = do
 --    mapM_ (putStrLn . showPt) (map tweak [V2 0 0, V2 0 1, V2 1 1, V2 1 0])
